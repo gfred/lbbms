6e358605cfb1a22453aa81503432948c08ea11e9
frederik.goetz@gmail.com <frederik.goetz@gmail.com@a8b9e4b6-6e45-f7c9-7cc7-8d46f47287dc>
2011-02-16 22:55:06 +0000

    message layout and send in android

--- de.gfred.lbbms.mobile/src/de/gfred/lbbms/mobile/util/Converter.java
+++ de.gfred.lbbms.mobile/src/de/gfred/lbbms/mobile/util/Converter.java
@@ -20,4 +20,20 @@ public class Converter {
 
         return obj;
     }
+
+    public static JSONObject createMSGJsonObject(String msg) {
+        JSONObject obj = new JSONObject();
+
+        try {
+            obj.put(Values.MSG_SENDATE, System.currentTimeMillis());
+            obj.put(Values.MSG_RECEIVER, "ALL");
+            obj.put(Values.MSG_CONTENT, msg);
+            obj.put(Values.MSG_LOCATION, convertLocationDataIntoJSONObject(new LocationData(2.0, 2.0, 123456789L)));
+            obj.put(Values.MSG_TYPE, "BROADCAST");
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+
+        return obj;
+    }
 }
--- de.gfred.lbbms.mobile/src/de/gfred/lbbms/mobile/util/Values.java
+++ de.gfred.lbbms.mobile/src/de/gfred/lbbms/mobile/util/Values.java
@@ -6,6 +6,7 @@ public interface Values {
     String SERVER_URI = "http://10.0.2.2:41584/de.gfred.lbbms.service";
     // String CUSTOMER_URI = "http://10.0.2.2:41584/de.gfred.lbbms.service/customer/1/location";
     String CUSTOMER_URI = "http://lbbms.dyndns.org:34900/de.gfred.lbbms.service/customer/1/location";
+    String MESSAGE_URI = "http://lbbms.dyndns.org:34900/de.gfred.lbbms.service/customer/1/messages";
 
     String HEADER_ACCEPT = "accept";
     String HEADER_TYPE = "content-type";
@@ -13,4 +14,12 @@ public interface Values {
 
     String JSON_LOCATION_LATITUDE = "lat";
     String JSON_LOCATION_LONGITUDE = "lon";
+
+    String MSG_LOCATION = "location";
+    String MSG_TYPE = "type";
+    String MSG_CONTENT = "content";
+    String MSG_RECEIVER = "receiver";
+    String MSG_SENDATE = "sendDate";
+
+    int SCHEME_PORT = 443;
 }
