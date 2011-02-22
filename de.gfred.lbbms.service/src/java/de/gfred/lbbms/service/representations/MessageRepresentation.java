9f99d255f06b855ddbd5da93ff0ef9492d2616e4
frederik.goetz@gmail.com <frederik.goetz@gmail.com@a8b9e4b6-6e45-f7c9-7cc7-8d46f47287dc>
2011-02-22 19:49:22 +0000

    Topic and durable subscriber doesn't work. Own message implementation. Some Rest classes changed. Model changed. Received Messages and send messages available!

--- de.gfred.lbbms.service/src/java/de/gfred/lbbms/service/representations/MessageRepresentation.java
+++ de.gfred.lbbms.service/src/java/de/gfred/lbbms/service/representations/MessageRepresentation.java
@@ -1,5 +1,6 @@
 package de.gfred.lbbms.service.representations;
 
+import de.gfred.lbbms.service.model.Message;
 import java.util.Date;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -14,18 +15,45 @@ import javax.xml.bind.annotation.XmlRootElement;
     </location>
 </message>
  */
+
 /**
  *
  * @author Frederik Goetz
  * @date 2011.02.03
  */
 @XmlRootElement(name="message")
-public class MessageRepresentation {
+public class MessageRepresentation implements Comparable<MessageRepresentation> {
     private String type;
     private String content;
     private LocationRepresentation location;
     private String receiver;
     private Date sendDate;
+    private String sender;
+
+    public MessageRepresentation(){
+    }
+
+    public MessageRepresentation(Message message){
+        type = message.getType();
+        content = message.getContent();
+
+        location = new LocationRepresentation();
+        location.setLat(message.getLocation().getLatitude());
+        location.setLon(message.getLocation().getLongitude());
+
+        sendDate = message.getSendDate();
+        sender = message.getCustomer().getName();
+        receiver = message.getReceiver();
+
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getSender() {
+        return sender;
+    }
 
     public String getContent() {
         return content;
@@ -66,5 +94,9 @@ public class MessageRepresentation {
     public void setType(String type) {
         this.type = type;
     }
-   
+
+    @Override
+    public int compareTo(MessageRepresentation o) {
+        return o.sendDate.compareTo(sendDate);
+    }
 }
--- de.gfred.lbbms.service/src/java/de/gfred/lbbms/service/resources/ReceivedMessagesRessource.java
+++ de.gfred.lbbms.service/src/java/de/gfred/lbbms/service/resources/ReceivedMessagesRessource.java
@@ -1,7 +1,30 @@
 package de.gfred.lbbms.service.resources;
 
+
+import de.gfred.lbbms.service.logic.interfaces.ICustomerAdministrationLocal;
+import de.gfred.lbbms.service.logic.interfaces.IMessageAdministrationLocal;
+import de.gfred.lbbms.service.model.Location;
+import de.gfred.lbbms.service.model.Message;
+import de.gfred.lbbms.service.representations.LocationRepresentation;
+import de.gfred.lbbms.service.representations.MessageRepresentation;
+import de.gfred.lbbms.service.resources.util.ResourceValues;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.jms.JMSException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -12,4 +35,26 @@ import javax.ws.rs.Path;
 @Path(ReceivedMessagesRessource.URI_TEMPALTE)
 public class ReceivedMessagesRessource {
     public static final String URI_TEMPALTE = MessagesRessource.URI_TEMPALTE + "/receive";
+
+    @EJB
+    private ICustomerAdministrationLocal customerBean;
+
+    @GET   
+    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public List<MessageRepresentation> getNewMessagesByLocation(@PathParam(ResourceValues.CUSTOMER_ID) final Long id){
+        if(id==null || customerBean.getCustomerById(id)==null){
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+
+        Set<Message> receivedMessages = customerBean.getCustomerById(id).getReceivedMessages();
+        List<MessageRepresentation> messages = new ArrayList<MessageRepresentation>();
+        
+        for(Message msg : receivedMessages){
+            messages.add(new MessageRepresentation(msg));
+        }
+
+        Collections.sort(messages);
+
+        return messages;
+    }
 }
